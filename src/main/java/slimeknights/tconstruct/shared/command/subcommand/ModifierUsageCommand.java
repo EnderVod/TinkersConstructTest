0a4fda9c71480f9261920ea239de8fe5e2da8ca4
EnderVod <jhonatanbr08@gmail.com>
2026-09-02 11:31:51 -0300

    Migrate modifier usage table helper to NeoForge

--- src/main/java/slimeknights/tconstruct/shared/command/subcommand/ModifierUsageCommand.java
+++ src/main/java/slimeknights/tconstruct/shared/command/subcommand/ModifierUsageCommand.java
@@ -7,7 +7,7 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
-import net.minecraftforge.common.util.TablePrinter;
+import net.neoforged.neoforge.common.util.TablePrinter;
 import slimeknights.mantle.command.MantleCommand;
 import slimeknights.mantle.util.RegistryHelper;
 import slimeknights.tconstruct.TConstruct;
@@ -47,17 +47,12 @@ public class ModifierUsageCommand {
   public static void register(LiteralArgumentBuilder<CommandSourceStack> subCommand) {
     subCommand.requires(sender -> sender.hasPermission(MantleCommand.PERMISSION_EDIT_SPAWN))
               .executes(context -> runForType(context, ModifierUsages.ALL, null))
-              // modifier_usage all
               .then(Commands.literal("all").executes(context -> runForType(context, ModifierUsages.ALL, null)))
-              // modifier_usage recipe [<slot_type>]
               .then(Commands.literal("recipe")
                             .then(Commands.argument("slot_type", SlotTypeArgument.slotType()).executes(ModifierUsageCommand::runRecipeWithFilter))
                             .executes(context -> runForType(context, ModifierUsages.RECIPE, null)))
-              // modifier_usage material_trait
               .then(Commands.literal("material_trait").executes(context -> runForType(context, ModifierUsages.MATERIAL_TRAIT, null)))
-              // modifier_usage tool_trait
               .then(Commands.literal("tool_trait").executes(context -> runForType(context, ModifierUsages.TOOL_TRAIT, null)))
-              // modifier_usage unused
               .then(Commands.literal("unused").executes(context -> runForType(context, ModifierUsages.UNUSED, null)));
   }
 
@@ -67,7 +62,6 @@ public class ModifierUsageCommand {
   }
 
   private static int runForType(CommandContext<CommandSourceStack> context, ModifierUsages filter, @Nullable OptionalSlotType slotFilter) {
-    // material traits are used in material traits (kinda obvious)
     IMaterialRegistry matReg = MaterialRegistry.getInstance();
     Set<Modifier> materialTraits = matReg.getAllMaterials().stream()
                                          .flatMap(mat -> {
@@ -79,7 +73,6 @@ public class ModifierUsageCommand {
                                          })
                                          .map(ModifierEntry::getModifier)
                                          .collect(Collectors.toSet());
-    // finally, tool traits we limit to anything in the modifiable tag
     Set<Modifier> toolTraits = RegistryHelper.getTagValueStream(BuiltInRegistries.ITEM, TinkerTags.Items.MODIFIABLE)
                                              .filter(item -> item instanceof IModifiable)
                                              .flatMap(item -> {
@@ -88,13 +81,10 @@ public class ModifierUsageCommand {
                                              })
                                              .map(ModifierEntry::getModifier)
                                              .collect(Collectors.toSet());
-    // TODO: modifier traits
 
-    // next, get our list of modifiers
     Stream<Modifier> modifierStream;
     switch (filter) {
       case RECIPE:
-        // filter to just one type of modifier if requested
         if (slotFilter != null) {
           modifierStream = ModifierRecipeLookup.getRecipeModifiers(slotFilter.slotType());
         } else {
@@ -111,12 +101,10 @@ public class ModifierUsageCommand {
         modifierStream = ModifierManager.INSTANCE.getAllValues();
         break;
     }
-    // if requested, filter out all
     if (filter == ModifierUsages.UNUSED) {
       modifierStream = modifierStream.filter(modifier -> !ModifierRecipeLookup.isRecipeModifier(modifier.getId()) && !materialTraits.contains(modifier) && !toolTraits.contains(modifier));
     }
 
-    // start building the table for output
     TablePrinter<ModifierUsageRow> table = new TablePrinter<>();
     table.header("ID", r -> r.modifierId().toString());
     if (filter != ModifierUsages.UNUSED) {
@@ -141,10 +129,8 @@ public class ModifierUsageCommand {
     }
     logOutput.append(System.lineSeparator());
 
-    // for all the modifiers (sorted), add table rows
     Collection<Modifier> finalList = modifierStream.sorted(Comparator.comparing(Modifier::getId)).toList();
     finalList.forEach(modifier -> {
-      // determine which recipes use this by slot type
       List<String> recipeUsages = SlotType.getAllSlotTypes().stream()
                                           .filter(type -> ModifierRecipeLookup.isRecipeModifier(type, modifier.getId()))
                                           .map(SlotType::getName)
@@ -158,7 +144,6 @@ public class ModifierUsageCommand {
       table.add(new ModifierUsageRow(modifier.getId(), recipes, toolTraits.contains(modifier), materialTraits.contains(modifier)));
     });
 
-    // finally, output the table
     table.build(logOutput);
     TConstruct.LOG.info(logOutput.toString());
     context.getSource().sendSuccess(() -> SUCCESS, true);
@@ -177,8 +162,6 @@ public class ModifierUsageCommand {
     private final String logPrefix;
   }
 
-  /**
-   * Data class holding a single row of the output table
-   */
+  /** Data class holding a single row of the output table */
   private record ModifierUsageRow(ModifierId modifierId, String recipe, boolean toolTrait, boolean materialTrait) {}
 }
