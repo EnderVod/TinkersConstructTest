1898bbb930977213ac1d3651d5ae5cba23d34d26
EnderVod <jhonatanbr08@gmail.com>
2026-09-02 11:33:59 -0300

    Migrate part texture command to NeoForge server arguments

--- src/main/java/slimeknights/tconstruct/shared/command/subcommand/GeneratePartTexturesCommand.java
+++ src/main/java/slimeknights/tconstruct/shared/command/subcommand/GeneratePartTexturesCommand.java
@@ -7,7 +7,7 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.server.command.ModIdArgument;
+import net.neoforged.neoforge.server.command.ModIdArgument;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.common.network.TinkerNetwork;
 import slimeknights.tconstruct.library.materials.definition.MaterialId;
@@ -25,7 +25,6 @@ public class GeneratePartTexturesCommand {
    */
   public static void register(LiteralArgumentBuilder<CommandSourceStack> subCommand) {
     subCommand.requires(source -> source.getEntity() instanceof ServerPlayer)
-              // generate_part_textures all|missing [<mod_id>|<material>]
               .then(Commands.literal("all")
                             .executes(context -> run(context, Operation.ALL, "", ""))
                             .then(Commands.argument("mod_id", ModIdArgument.modIdArgument()).executes(context -> runModId(context, Operation.ALL)))
@@ -36,18 +35,15 @@ public class GeneratePartTexturesCommand {
                             .then(Commands.argument("material", MaterialArgument.material()).executes(context -> runMaterial(context, Operation.MISSING))));
   }
 
-  /** Runs the command, filtered by a material */
   private static int runMaterial(CommandContext<CommandSourceStack> context, Operation filter) throws CommandSyntaxException {
     MaterialId material = MaterialArgument.getMaterial(context, "material").getIdentifier();
     return run(context, filter, material.getNamespace(), material.getPath());
   }
 
-  /** Runs the command, filtered by a mod ID */
   private static int runModId(CommandContext<CommandSourceStack> context, Operation filter) throws CommandSyntaxException {
     return run(context, filter, context.getArgument("mod_id", String.class), "");
   }
 
-  /** Runs the command */
   private static int run(CommandContext<CommandSourceStack> context, Operation filter, String modId, String materialName) throws CommandSyntaxException {
     CommandSourceStack source = context.getSource();
     source.sendSuccess(() -> SUCCESS, true);
