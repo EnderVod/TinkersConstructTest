bd0f2ebbffec05978de901a5926ba44ca33e1dee
EnderVod <jhonatanbr08@gmail.com>
2026-09-02 10:25:38 -0300

    Migrate smeltery display fluids to NeoForge

--- src/main/java/slimeknights/tconstruct/smeltery/block/entity/tank/IDisplayFluidListener.java
+++ src/main/java/slimeknights/tconstruct/smeltery/block/entity/tank/IDisplayFluidListener.java
@@ -1,13 +1,13 @@
 package slimeknights.tconstruct.smeltery.block.entity.tank;
 
-import net.minecraftforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.FluidStack;
 
 /**
  * Interface for blocks to be notified when the smeltery has a new bottommost fluid
  */
 public interface IDisplayFluidListener {
   /**
-   * Called when the display fluid changes0
+   * Called when the display fluid changes
    * @param fluid New display fluid, is safe to store (will not be modified)
    */
   void notifyDisplayFluidUpdated(FluidStack fluid);
