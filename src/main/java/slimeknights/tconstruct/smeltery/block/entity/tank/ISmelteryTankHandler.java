08fec50aaa118ae87110f120fd989be9673df398
EnderVod <jhonatanbr08@gmail.com>
2026-09-02 10:27:10 -0300

    Use NeoForge fluid types in smeltery tank interface

--- src/main/java/slimeknights/tconstruct/smeltery/block/entity/tank/ISmelteryTankHandler.java
+++ src/main/java/slimeknights/tconstruct/smeltery/block/entity/tank/ISmelteryTankHandler.java
@@ -1,46 +1,32 @@
 package slimeknights.tconstruct.smeltery.block.entity.tank;
 
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 
 import java.util.List;
 
-// TODO: reassess
+/** Common tank access used by smeltery controllers and their client sync. */
 public interface ISmelteryTankHandler {
-  /**
-   * Updates the fluids in the tank with data from the packet, should only be called client side
-   */
+  /** Updates the fluids in the tank with data from the packet, should only be called client side. */
   void updateFluidsFromPacket(List<FluidStack> fluids);
 
-  /**
-   * Gets the smeltery tank
-   * @return  Tank instance
-   */
+  /** Gets the smeltery tank. */
   SmelteryTank<?> getTank();
 
   /**
-   * Gets the fluid capability for this smeltery. Provided here as the smeltery itself does not expose this
-   * @return  Fluid capability
+   * Legacy internal holder used by drains while the multiblock capability path is migrated.
+   * External capability exposure is being moved to NeoForge RegisterCapabilitiesEvent providers.
    */
   LazyOptional<IFluidHandler> getFluidCapability();
 
-  /**
-   * Called when the tank adds or removes a fluid to notify listeners
-   * @param type   Type of the change
-   * @param fluid  Fluid changed, may be empty if multiple fluids change (order change for example)
-   */
+  /** Called when the tank adds or removes a fluid to notify listeners. */
   default void notifyFluidsChanged(FluidChange type, FluidStack fluid) {}
 
-  /** Simple enum to make {@link #notifyFluidsChanged(FluidChange, FluidStack)} more readible */
   enum FluidChange {
-    /** Fluid was added to the tank */
     ADDED,
-    /** Fluid size changed */
     CHANGED,
-    /** Fluid was removed from the block */
     REMOVED,
-    /** Sent client side to signify the bottom most fluid is different */
     ORDER_CHANGED
   }
 }
