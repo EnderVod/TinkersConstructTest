62500a22d869ff5a8c2188f6e30231d2a02ac9b1
EnderVod <jhonatanbr08@gmail.com>
2026-09-02 23:01:49 -0300

    Migrate modifier loot condition codec

--- src/main/java/slimeknights/tconstruct/tools/modifiers/loot/HasModifierLootCondition.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/loot/HasModifierLootCondition.java
@@ -1,25 +1,28 @@
 package slimeknights.tconstruct.tools.modifiers.loot;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
-import lombok.RequiredArgsConstructor;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.Serializer;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
-import slimeknights.mantle.util.JsonHelper;
 import slimeknights.tconstruct.common.TinkerTags;
 import slimeknights.tconstruct.library.modifiers.ModifierId;
 import slimeknights.tconstruct.library.tools.helper.ModifierUtil;
 import slimeknights.tconstruct.tools.TinkerModifiers;
 
 /** Condition to check if a held tool has the given modifier */
-@RequiredArgsConstructor
 public class HasModifierLootCondition implements LootItemCondition {
+  public static final MapCodec<HasModifierLootCondition> CODEC = ResourceLocation.CODEC
+    .xmap(id -> new HasModifierLootCondition(new ModifierId(id)), condition -> condition.modifier)
+    .fieldOf("modifier");
+
   private final ModifierId modifier;
+
+  public HasModifierLootCondition(ModifierId modifier) {
+    this.modifier = modifier;
+  }
 
   @Override
   public LootItemConditionType getType() {
@@ -30,17 +33,5 @@ public class HasModifierLootCondition implements LootItemCondition {
   public boolean test(LootContext context) {
     ItemStack tool = context.getParamOrNull(LootContextParams.TOOL);
     return tool != null && tool.is(TinkerTags.Items.MODIFIABLE) && ModifierUtil.getModifierLevel(tool, modifier) > 0;
-  }
-
-  public static class ConditionSerializer implements Serializer<HasModifierLootCondition> {
-    @Override
-    public void serialize(JsonObject json, HasModifierLootCondition condition, JsonSerializationContext context) {
-      json.addProperty("modifier", condition.modifier.toString());
-    }
-
-    @Override
-    public HasModifierLootCondition deserialize(JsonObject json, JsonDeserializationContext context) {
-      return new HasModifierLootCondition(new ModifierId(JsonHelper.getResourceLocation(json, "modifier")));
-    }
   }
 }
