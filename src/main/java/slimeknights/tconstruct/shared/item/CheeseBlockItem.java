3b7e3845cec609cb106bbc475c64e21e396ec753
EnderVod <jhonatanbr08@gmail.com>
2026-09-04 10:08:33 -0300

    Fix CheeseBlockItem tooltip import

--- src/main/java/slimeknights/tconstruct/shared/item/CheeseBlockItem.java
+++ src/main/java/slimeknights/tconstruct/shared/item/CheeseBlockItem.java
@@ -4,6 +4,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
