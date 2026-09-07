f6c38a7586e489e68a2831db70787f1e18ea4530
EnderVod <jhonatanbr08@gmail.com>
2026-09-07 02:22:57 -0300

    Restore material ID inheritance contract

--- src/main/java/slimeknights/tconstruct/library/materials/definition/MaterialId.java
+++ src/main/java/slimeknights/tconstruct/library/materials/definition/MaterialId.java
@@ -60,7 +60,7 @@ public final class MaterialId extends ResourceId implements MaterialVariantId {
 
   @Override
   public ResourceLocation getLocation(char separator) {
-    return location();
+    return this;
   }
 
   @Override
